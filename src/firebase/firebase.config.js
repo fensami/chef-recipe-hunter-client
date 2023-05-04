62f9505bab9b97eb1d003e17c802b6d54973a4f3
fensami <113832498+fensami@users.noreply.github.com>
2023-05-04 16:46:24 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log("environment variable", import.meta.env.VITE_PASS);
 const firebaseConfig = {
-  apiKey: "AIzaSyB3ojYispfGUv-jGO2JJ8ZgckLe455iKxQ",
-  authDomain: "foodian-7ebc2.firebaseapp.com",
-  projectId: "foodian-7ebc2",
-  storageBucket: "foodian-7ebc2.appspot.com",
-  messagingSenderId: "1003451622996",
-  appId: "1:1003451622996:web:40155da493b2ec3685afc1"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
--- src/pages/specialFoods/SpecialFoods.jsx
+++ src/pages/specialFoods/SpecialFoods.jsx
@@ -38,7 +38,7 @@ const SpecialFoods = () => {
         <Card>
           <Card.Img className='h-50' variant="top" src={special3} />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Chicken Masala</Card.Title>
             <Card.Text>
               This is a wider card with supporting text below as a natural lead-in
               to additional content. This card has even longer content than the
